9d16905a4a2fcd4da4db83c5e696eb642eb11b7a
Remezov Dmitriy <darknessdizi@gmail.com>
2023-06-23 17:03:33 +0800

    В задаче 3 добавлено решение с навигацией по точкам

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,7 @@
 const img = document.querySelectorAll('img'),
     arrowPrev = document.querySelector('.slider__arrow_prev'),
-    arrowNext = document.querySelector('.slider__arrow_next');
+    arrowNext = document.querySelector('.slider__arrow_next'),
+    dot = document.querySelectorAll('.slider__dot');
 let index = 0;
 
 function replaceActiveSlider(oldIndex, newIndex) {
@@ -8,22 +9,30 @@ function replaceActiveSlider(oldIndex, newIndex) {
     parent.className = "slider__item";
     parent = img[newIndex].parentElement;
     parent.className = "slider__item slider__item_active";
+
+    // подсветка точек навигации:
+    dot[oldIndex].className = "slider__dot";
+    dot[index].className = "slider__dot slider__dot_active";
 };
 
 arrowPrev.onclick = () => {
     const oldIndex = index;
-    if (index == 0) {
-        index = img.length;
-    }
+    if (index == 0) {index = img.length;};
     --index;
     replaceActiveSlider(oldIndex, index);
 }
 
 arrowNext.onclick = () => {
     const oldIndex = index;
-    if (index + 1 == img.length) {
-        index = -1;
-    }
+    if (index + 1 == img.length) {index = -1;};
     ++index;
     replaceActiveSlider(oldIndex, index);
 }
+
+dot.forEach((element, key) => {
+    element.onclick = () => {
+        const oldIndex = index;
+        index = key;
+        replaceActiveSlider(oldIndex, index);
+    };
+});
