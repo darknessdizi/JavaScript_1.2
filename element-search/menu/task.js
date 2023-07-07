64ec68ae6e3eda40b84c93c2c6e37ddbd55993b2
Remezov Dmitriy <darknessdizi@gmail.com>
2023-07-07 14:38:58 +0700

    Доработана задача с меню, добавлена возможность закрытия при повторном нажатии

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,21 @@
 const links = document.querySelectorAll('.menu__link');
-console.log(links);
 
 links.forEach((element) => {
     const parent = element.parentElement;
     if (parent.getElementsByClassName('menu menu_sub').length) {
         const subMenu = parent.querySelector('.menu_sub');
         element.onclick = () => {
-            const subMenuAll = document.querySelectorAll('.menu_sub');
-            subMenuAll.forEach((item) => item.className = 'menu menu_sub')
-            subMenu.className = 'menu menu_sub menu_active';
+            if (subMenu.className.includes('menu_active')) {
+                subMenu.className = 'menu menu_sub';
+            } else {
+                const menu = element.closest('.menu_main');
+                const subMenuOld = menu.querySelector('.menu_active');
+                if (subMenuOld) {
+                    subMenuOld.className = 'menu menu_sub';
+                }
+                subMenu.className = 'menu menu_sub menu_active';
+            }
             return false;
         };
-    };
+    }
 });
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,7 +1,7 @@
-const img = document.querySelectorAll('img'),
-    arrowPrev = document.querySelector('.slider__arrow_prev'),
-    arrowNext = document.querySelector('.slider__arrow_next'),
-    dot = document.querySelectorAll('.slider__dot');
+const img = document.querySelectorAll('img');
+const arrowPrev = document.querySelector('.slider__arrow_prev');
+const arrowNext = document.querySelector('.slider__arrow_next');
+const dot = document.querySelectorAll('.slider__dot');
 let index = 0;
 
 function replaceActiveSlider(oldIndex, newIndex) {
@@ -13,21 +13,21 @@ function replaceActiveSlider(oldIndex, newIndex) {
     // подсветка точек навигации:
     dot[oldIndex].className = "slider__dot";
     dot[index].className = "slider__dot slider__dot_active";
-};
+}
 
 arrowPrev.onclick = () => {
     const oldIndex = index;
-    if (index == 0) {index = img.length;};
+    if (index == 0) {index = img.length;}
     --index;
     replaceActiveSlider(oldIndex, index);
-}
+};
 
 arrowNext.onclick = () => {
     const oldIndex = index;
-    if (index + 1 == img.length) {index = -1;};
+    if (index + 1 == img.length) {index = -1;}
     ++index;
     replaceActiveSlider(oldIndex, index);
-}
+};
 
 dot.forEach((element, key) => {
     element.onclick = () => {
