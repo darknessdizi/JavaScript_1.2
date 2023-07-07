508dc2cf8d6f0abf9ce352f08c29290e88f838f2
Remezov Dmitriy <darknessdizi@gmail.com>
2023-07-07 16:49:21 +0700

    Отредактирована задача 3. Слайдеры независимы, нет глобальных переменных

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,60 @@
-const img = document.querySelectorAll('img');
-const arrowPrev = document.querySelector('.slider__arrow_prev');
-const arrowNext = document.querySelector('.slider__arrow_next');
-const dot = document.querySelectorAll('.slider__dot');
-let index = 0;
-
-function replaceActiveSlider(oldIndex, newIndex) {
-    let parent = img[oldIndex].parentElement;
+function replaceActiveSlider(slider, oldIndex, newIndex) {
+    const imgs = slider.querySelectorAll('img');
+    let parent = imgs[oldIndex].parentElement;
     parent.className = "slider__item";
-    parent = img[newIndex].parentElement;
+    parent = imgs[newIndex].parentElement;
     parent.className = "slider__item slider__item_active";
 
     // подсветка точек навигации:
+    const dot = slider.querySelectorAll('.slider__dot');
     dot[oldIndex].className = "slider__dot";
-    dot[index].className = "slider__dot slider__dot_active";
+    dot[newIndex].className = "slider__dot slider__dot_active";
 }
 
-arrowPrev.onclick = () => {
-    const oldIndex = index;
-    if (index == 0) {index = img.length;}
-    --index;
-    replaceActiveSlider(oldIndex, index);
-};
+function main() {
+    const slider = document.querySelectorAll('.slider');
+    slider.forEach((element) => {
+        const imgs = element.querySelectorAll('img');
+        const imgsArray = Array.from(imgs);
+        let index = imgsArray.findIndex((value) => {
+            const parent = value.parentElement;
+            if (parent.className.includes('active')) {
+                return true;
+            } else {
+                return false;
+            }
+        });
 
-arrowNext.onclick = () => {
-    const oldIndex = index;
-    if (index + 1 == img.length) {index = -1;}
-    ++index;
-    replaceActiveSlider(oldIndex, index);
-};
+        const arrowPrev = element.querySelector('.slider__arrow_prev');
+        arrowPrev.onclick = () => { 
+            const oldIndex = index;
+            if (index == 0) {
+                index = imgs.length;
+            }
+            --index;
+            replaceActiveSlider(element, oldIndex, index);
+        };
 
-dot.forEach((element, key) => {
-    element.onclick = () => {
-        const oldIndex = index;
-        index = key;
-        replaceActiveSlider(oldIndex, index);
-    };
-});
+        const arrowNext = element.querySelector('.slider__arrow_next');
+        arrowNext.onclick = () => {
+            const oldIndex = index;
+            if (index + 1 == imgs.length) {
+                index = -1;
+            }
+            ++index;
+            replaceActiveSlider(element, oldIndex, index);
+        };
+
+        const dot = element.querySelectorAll('.slider__dot');
+        dot.forEach((value, key) => {
+            value.onclick = () => {
+                const oldIndex = index;
+                index = key;
+                replaceActiveSlider(element, oldIndex, index);
+            };
+        });
+    });
+}
+
+
+main();
